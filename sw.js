74ef12f7556ac519947e520711737782a36e405c
Ariake913 <ariakekosaka@gmail.com>
2026-03-16 16:52:54 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,11 @@
-const CACHE='atashi-v1';
-const ASSETS=['/','/index.html','/manifest.json','/icon.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+const CACHE='atashi-v3';
+const ASSETS=['/atashi/','/atashi/index.html','/atashi/manifest.json','/atashi/icon.png'];
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
